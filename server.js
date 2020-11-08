4eb2dcf18a8a45ae589e2004d04840105e1b3b4a
unknown <ismailajizoumo@gmail.com>
2020-11-08 15:03:56 +0100

    setting up the changeProfilepic endpoint and added a new column in the database as profile image

--- server.js
+++ server.js
@@ -11,25 +11,16 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 const profileChanger = require('./controllers/profileChanger');
+
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'test',
-    database : 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
-
-// const db = knex({
-//   client: 'pg',
-//   connection: {
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//       rejectUnauthorized: false
-//     }
-//   }
-// });
 const app =express();
 
 
