23d019ad895f6c6b94e7214c04773d35170748ed
Ismail Ajizou <64745987+ismailajizou@users.noreply.github.com>
2021-04-28 11:48:59 +0000

    Update profileChanger.js
    fixing change profile request

--- controllers/profileChanger.js
+++ controllers/profileChanger.js
@@ -1,16 +1,19 @@
 const sharp = require('sharp');
 
 const changeProfilePic = (req, res, db) => {
-    if((req.file.mimetype === 'image/jpeg' 
-    || req.file.mimetype === 'image/png')
-    && req.file.size <= 100000){
-        sharp(req.file.buffer).resize({
+    const image = req.file;
+    const { id } = req.params;
+    if((image.mimetype === 'image/jpeg' 
+    || image.mimetype === 'image/png')
+    && image.size <= 1024 * 100){
+        sharp(image.buffer).resize({
             width: 100,
             height: 100,
             position: 'center',
         }).toBuffer().then(buffer => 
             db('users')
             .update({ profileimage: buffer })
+            .where({id})
             .returning('profileimage')
             .then(response =>  res.json(response))
             .catch(err => res.json("Ooops! something went wrong!"))
