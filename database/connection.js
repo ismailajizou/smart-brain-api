4148c6e1ba5f4c173120fc1adfaa5e914a2a1436
unknown <ismailajizoumo@gmail.com>
2021-06-16 16:35:38 +0100

    dynamically connecte to database

--- database/connection.js
+++ database/connection.js
@@ -1,20 +1,18 @@
-// const db = require('knex')({
-//   client: 'pg',
-//   connection: {
-//     host : '127.0.0.1',
-//     user : 'postgres',
-//     password : 'test',
-//     database : 'brain'
-//   }
-// });
+const connection = process.env.NODE_ENV === "development" ? {
+  host : '127.0.0.1',
+  user : 'postgres',
+  password : 'test',
+  database : 'brain'
+} : {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+}
+
 
 const db = require('knex')({
     client: 'pg',
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    }
+    connection: connection
 });
 module.exports = db;
