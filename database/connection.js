41826b1736940740243ab372372903b7acb22bfc
unknown <ismailajizoumo@gmail.com>
2021-09-13 19:30:57 +0100

    add env vars and remove body-parser

--- database/connection.js
+++ database/connection.js
@@ -1,19 +1,21 @@
-const connection = process.env.NODE_ENV === "dev" ? 
-({
+const devModeConnection  = {
   host : '127.0.0.1',
-  user : 'postgres',
-  password : 'test',
-  database : 'brain'
-}) : ({
+  user : process.env.DATABASE_USER,
+  password : process.env.DATABASE_PWD,
+  database : process.env.DATABASE_NAME
+}
+const prodModeConnection = {
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
-})
+}
+
+const connection = process.env.NODE_ENV === "dev" ? devModeConnection : prodModeConnection;
 
 const db = require('knex')({
-    client: 'pg',
-    connection: connection
+  client: 'pg',
+  connection: connection
 });
 
 module.exports = db;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
+process.env.NODE_ENV === "dev" && require("dotenv").config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const bcrypt= require('bcrypt-nodejs');
 const cors = require('cors');
 const multer = require('multer');
@@ -12,11 +12,20 @@ const { handleApiCall, handleImage } = require('./controllers/image');
 const changeProfilePic = require('./controllers/profileChanger');
 const deleteAccount = require('./controllers/deleteAccount');
 
-const app =express();
-app.use(cors());
-app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send(db.user));
+// Setting up the Router
+const app = express();
+// Defining the port where to run the up
+// In devMode the port is 3000
+// But In prod it will be the prod port
+const port = process.env.PORT ?? 3000;
+
+// Setting up middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes (endpoints) of the API
+app.get('/', (_req, res) => res.send("Smart Brain Api"));
 app.post("/signin", (req, res) => handleSignin(req, res, db, bcrypt));
 app.post('/register', (req, res) => handleRegister(req, res, db, bcrypt));
 app.post('/imageUrl', (req, res) => handleApiCall(req, res));
@@ -24,8 +33,10 @@ app.put('/image', (req, res) => handleImage(req, res, db));
 app.post('/changeProfilePic/:id',  upload, (req, res) => changeProfilePic(req, res, db));
 app.post('/deleteAccount/:id', (req, res) => deleteAccount(req, res, db, bcrypt));
 
+// Handling any request other than the above
 app.all('*', (req, res) => res.status(404).send("Page Not Found"));
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`app running on port ${process.env.PORT ? process.env.PORT : 3000}`);
-})
+// Listening to the default port
+app.listen(port, () => {
+  console.log(`app running on port ${port}`);
+});
